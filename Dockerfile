cfc3158bdc023e7550551a36ed0ce5ef112ba667
inpure <chfjinqh@gmail.com>
2022-03-11 17:14:01 +0800

    add sub commands: greet

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM golang:latest
 COPY ./main.go /opt
 RUN cd /opt && go mod tidy && go build ./main.go
 EXPOSE 54321
-ENTRYPOINT ["./go2docker"]
+ENTRYPOINT ["/opt/main"]
--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"flag"
+	"github.com/spf13/cobra"
 	"log"
 	"time"
 )
 
 func main() {
-	for {
-		log.Println("Hello World")
-		time.Sleep(2 * time.Second)
+	var pause int
+	var times int
+	var name string
+
+	flag.IntVar(&pause, "p", 1, "pause time，default 1s")
+	flag.IntVar(&times, "t", 1, "output times，default 1")
+	flag.StringVar(&name, "n", "World", "name，default \"World\"")
+	flag.Parse() //parse the command line arguments
+
+	var cmdGreet = &cobra.Command{
+		Use:   "greet",
+		Short: "name",
+		Long:  "greet name",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < times; i++ {
+				log.Println("Hello " + name)
+				if i == times-1 {
+					break
+				}
+				time.Sleep(time.Duration(pause) * time.Second)
+			}
+		},
 	}
+
+	var rootCmd = &cobra.Command{Use: "hello"}
+	rootCmd.AddCommand(cmdGreet)
+	rootCmd.Execute()
 }
